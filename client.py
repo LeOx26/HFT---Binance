b6cd8da05b3eda08957cffed2ec0554d7f344a1f
Eabh09 <80545475+Eabh09@users.noreply.github.com>
2022-11-25 20:31:07 -0500

    Update client.py

--- client.py
+++ client.py
@@ -2,8 +2,8 @@
 from binance.client import Client
 #Insert API and Secret key in quotation marks
 class client():
- api_key = 'kr41YsYIbL19a44v3vjifmD2vm6B0nldyQE6GQ7ulShpNpP4GdHU3Yl25Tr82TgO'
- api_secret = 'TbqgIWkv45W42MDkW2cs1GYwhZGRteIBf38SeVKym85OnrszPalUyY16S7CjiO32'
+ api_key = ''
+ api_secret = ''
  client = Client(api_key, api_secret)
 
  candles = client.get_klines(symbol='BNBBTC', interval=Client.KLINE_INTERVAL_30MINUTE)
